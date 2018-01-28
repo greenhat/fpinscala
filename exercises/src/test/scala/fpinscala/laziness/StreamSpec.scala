8fde31a52961ff77e627e3fba8ab70ef18ece64d
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-01-28 13:21:15 +0200

    Adds test for Stream.take to be lazy;

--- exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -15,6 +15,16 @@ class StreamSpec extends WordSpec {
       assert(Stream(1, 2, 3).take(4).toList == List(1, 2, 3))
     }
 
+    "take n is lazy" in {
+      var effect = 0
+      val stream = Stream.cons({ effect += 1; 1 }, Stream(2, 3))
+      assert(effect == 0)
+      val takenStream = stream.take(1)
+      assert(effect == 0)
+      assert(takenStream.toList == List(1))
+      assert(effect == 1)
+    }
+
     "drop n" in {
       assert(Stream(1, 2, 3).drop(1).toList == List(2, 3))
       assert(Stream(1, 2, 3).drop(2).toList == List(3))
