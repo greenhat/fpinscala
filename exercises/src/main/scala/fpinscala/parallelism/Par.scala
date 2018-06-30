6c57871a9cce3e0f848b9942e3a2b2659fc16c93
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-06-30 18:06:51 +0300

    add lazyUnit and asyncF;

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -1,6 +1,9 @@
 package fpinscala.parallelism
 
 import java.util.concurrent._
+
+import fpinscala.parallelism.Nonblocking.Par.ParOps
+
 import language.implicitConversions
 
 object Par {
@@ -44,6 +47,12 @@ object Par {
     es => 
       if (run(es)(cond).get) t(es) // Notice we are blocking on the result of `cond`.
       else f(es)
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] = { a: A =>
+    lazyUnit(f(a))
+  }
 
   /* Gives us infix syntax for `Par`. */
   implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
